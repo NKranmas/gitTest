7379f6f25dcd0bc6369cb9c712b1211784e7d55c
Kranmas <njkranmas@labs.neit.edu>
2019-01-17 13:51:47 -0500

    commit

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
-console.log("This is our test for gitHub");
-console.log("This is our test for gitHub");
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var timestamp = require('./timeStamp');
+var nodeEvents = require('./nodeEvents');
+
+
+http.createServer(function(req, res){
+    /*//file system response
+    fs.readFile('index.html', function(err, data){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write(data);
+        res.end();
+    });*/
+    
+
+    //nodeEvents.evetEmitter.emit(message);
+
+    res.write("<h1>Welcome to the Homepage </h1>");
+    res.write("The date and time is: " + timestamp.timeStamp() + "<br>");
+    res.write("This is the url you requested " + req.url);
+    var q = url.parse(req.url, true).query;
+    var resTxt = q.id + " " + q.user;
+    var qList = url.parse(req.url, true);
+    //console.log(qList);
+    console.log(qList.host);
+    console.log(qList.pathname);
+    console.log(qList.search);
+    res.end("<br>" + resTxt);
+}).listen(3030);
---
+++ nodeEvents.js
@@ -0,0 +1,14 @@
+var events = require('events');
+
+var eventEmitter = new events.EventEmitter();
+
+//result of event
+var message = function(){
+    console.log("You fired off an event");
+}
+
+
+//crates a listener
+eventEmitter.on("evnt", message);
+
+eventEmitter.emit('evnt');
